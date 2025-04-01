1a64e23f998b3260ff4c838869da1115ea088b64
TiagoMaiaL <tiago.maia.lopes@gmail.com>
2025-03-31 21:51:54 -0300

    Define empty tests for parser

--- tests/test-parser.c
+++ tests/test-parser.c
@@ -8,12 +8,47 @@
 
 static FILE *test_file_handle = NULL;
 
+void test_parsing_empty_class();
+void test_parsing_class_with_vars();
+void test_parsing_class_with_empty_funcs();
+void test_parsing_func_body();
+void test_parsing_expr();
+
 void test_parser()
 {
     tst_suite_begin("Parser");
 
-    //tst_unit("Empty class", );
+    tst_unit("Empty class", *test_parsing_empty_class);
+    tst_unit("Class with vars", *test_parsing_class_with_vars);
+    tst_unit("Class with empty funcs", *test_parsing_class_with_empty_funcs);
+    tst_unit("Func body", *test_parsing_func_body);
+    tst_unit("Expressions", *test_parsing_expr);
 
     tst_suite_finish();
+}
+
+void test_parsing_empty_class()
+{
+
+}
+
+void test_parsing_class_with_vars()
+{
+
+}
+
+void test_parsing_class_with_empty_funcs()
+{
+
+}
+
+void test_parsing_func_body()
+{
+
+}
+
+void test_parsing_expr()
+{
+
 }
 
